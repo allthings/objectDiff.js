6953435a79fffa7c1ec4231070d37f331b506db9
VadenPro1 <yariktiyarik@gmail.com>
2023-08-10 12:51:52 +0300

    add typescrypt declaration

---
+++ objectDiff.d.ts
@@ -0,0 +1,19 @@
+declare module 'deep-object-diff' {
+  export type ChangeType = 'equal' | 'primitive change' | 'removed' | 'added' | 'object change';
+
+  export interface DiffResult {
+    changed: ChangeType;
+    value: any;
+    removed?: any;
+    added?: any;
+  }
+
+  interface ObjectDiff {
+    diff: (a: object, b: object) => DiffResult;
+    diffOwnProperties: (a: object, b: object) => DiffResult;
+    convertToXMLString: (changes: DiffResult) => string;
+  }
+
+  const objectDiff: ObjectDiff;
+  export default objectDiff;
+}
